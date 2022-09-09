4fc8f8d6784a621087b7f8a2d9f1ca39d56b7246
jaekanglee <ksdy234@barogo.com>
2022-09-09 22:32:35 +0900

    Feat NetworModule 및 API Response 정의 - Retrofit , Logging, API Service, Di module - Response MovieDto

---
+++ 03_data/src/main/java/com/ppizil/data/di/NetworkModule.kt
@@ -0,0 +1,55 @@
+package com.ppizil.data.di
+
+import com.ppizil.service.MovieRemoteService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideLoggintInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        loggintInterCeptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            addInterceptor(loggintInterCeptor)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client : OkHttpClient
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .baseUrl("https://api.themoviedb.org/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+
+    @Provides
+    @Singleton
+    fun provideMovieRemoteService(
+        retrofit: Retrofit
+    ) : MovieRemoteService =
+        retrofit.create(MovieRemoteService::class.java)
+}

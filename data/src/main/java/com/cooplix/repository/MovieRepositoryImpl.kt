5fae3e496c537c1b4d4b351a7b19cb9835bd0bf5
saeyeong <syeongppp@naver.com>
2022-09-18 19:59:18 +0900

    api key 위치 수정

--- data/src/main/java/com/cooplix/repository/MovieRepositoryImpl.kt
+++ data/src/main/java/com/cooplix/repository/MovieRepositoryImpl.kt
@@ -1,25 +1,23 @@
 package com.cooplix.repository
 
-import com.cooplix.BuildConfig
 import com.cooplix.model.Movie
 import com.cooplix.model.MovieGenre
 import com.cooplix.remote.api.MovieService
 
 class MovieRepositoryImpl(private val movieService: MovieService): MovieRepository {
 
-    private val apiKey = BuildConfig.api_key
 
     override suspend fun getMovies(): List<Movie> {
-        return movieService.getMovie(apiKey).toMovies(getMovieGenres()) ?: emptyList()
+        return movieService.getMovie().toMovies(getMovieGenres()) ?: emptyList()
     }
 
     override suspend fun getMoviesGroup(): Map<MovieGenre, List<Movie>> {
-        val movies = movieService.getMovie(apiKey).toMovies(getMovieGenres())
+        val movies = movieService.getMovie().toMovies(getMovieGenres())
 
         return movies?.groupBy { it.genres[0] } ?: emptyMap()
     }
 
-    private suspend fun getMovieGenres(): List<MovieGenre>? = movieService.getMovieGenres(apiKey).genres?.mapNotNull {
+    private suspend fun getMovieGenres(): List<MovieGenre>? = movieService.getMovieGenres().genres?.mapNotNull {
         it.toMovieGenre()
     }
 }

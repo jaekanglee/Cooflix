58855773997fe4fc95f4322a1973307c272a9980
jaekanglee <ksdy234@barogo.com>
2022-09-19 13:07:36 +0900

    Refactor [ 카테고리 종류 추가 ]

--- 01_presentaion/src/main/java/com/ppizil/presentaion/movielist/MovieCategories.kt
+++ 01_presentaion/src/main/java/com/ppizil/presentaion/movielist/MovieCategories.kt
@@ -4,11 +4,18 @@ package com.ppizil.presentaion.movielist
 sealed class MovieCategories {
     object Action : MovieCategories()
     object Romantic : MovieCategories()
+    object Ect : MovieCategories()
+    object Test : MovieCategories()
+    object Horror : MovieCategories()
+
 }
 
 fun Int.mapToCategory() =
     when (this) {
         53 -> MovieCategories.Action
+        12 -> MovieCategories.Ect
+        28 -> MovieCategories.Test
+        16 -> MovieCategories.Horror
         else -> MovieCategories.Romantic
     }
 
@@ -18,5 +25,7 @@ fun MovieCategories.mapToStringType(): String? =
     when (this) {
         MovieCategories.Action -> "액션"
         MovieCategories.Romantic -> "로맨틱"
-        else -> "기타"
+        MovieCategories.Ect -> "기타"
+        MovieCategories.Horror -> "호러"
+        MovieCategories.Test -> "메롱"
     }

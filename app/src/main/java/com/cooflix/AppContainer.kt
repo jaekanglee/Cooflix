49ba14175609dfaed7336284c674072fef014f2c
saeyeong <syeongppp@naver.com>
2022-09-24 23:19:56 +0900

    수동 종속성 주입 AppContainer 추가

---
+++ app/src/main/java/com/cooflix/AppContainer.kt
@@ -0,0 +1,12 @@
+package com.cooflix
+
+import com.cooplix.remote.api.MovieServiceFactory
+import com.cooplix.repository.MovieRepository
+import com.cooplix.repository.MovieRepositoryImpl
+import com.cooplix.usecase.GetMoviesUseCase
+
+class AppContainer {
+    private val movieService = MovieServiceFactory.createService()
+    private val movieRepository: MovieRepository = MovieRepositoryImpl(movieService)
+    val getMoviesUseCase = GetMoviesUseCase(movieRepository)
+}
